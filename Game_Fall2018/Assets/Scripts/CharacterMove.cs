ba049ce7062da7cb45eb40b18fe9b3e631f6e96b
Preston Powell <preston.powell2020@gmail.com>
2018-09-10 17:15:04 -0600

    Created first script. CharacterMove.cs

---
+++ Game_Fall2018/Assets/Scripts/CharacterMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour {
+
+	public int MoveSpeed;
+	public float JumpHeight;
+
+	// Use this for initialization
+	void Start () {
+		print("Hello World!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
