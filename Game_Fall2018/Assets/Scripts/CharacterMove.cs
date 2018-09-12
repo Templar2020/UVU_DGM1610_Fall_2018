2e67b99ace7d5e4261b05a960d0ba58caf637552
Preston Powell <preston.powell2020@gmail.com>
2018-09-12 17:16:40 -0600

    Created character move script

--- Game_Fall2018/Assets/Scripts/CharacterMove.cs
+++ Game_Fall2018/Assets/Scripts/CharacterMove.cs
@@ -1,19 +1,49 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class CharacterMove : MonoBehaviour {
 
+	// Player Movement Variables
 	public int MoveSpeed;
 	public float JumpHeight;
 
+	// Player grounded variables
+	public Transform GroundCheck;
+	public float GroundCheckRadius;
+	public LayerMask WhatIsGround;
+	private bool Grounded;
+
 	// Use this for initialization
 	void Start () {
-		print("Hello World!");
+	
 	}
 	
+
+	void FixedUpdate () {
+		Grounded = Physics2D.OverlapCircle(GroundCheck.position, GroundCheckRadius, WhatIsGround);
+	}
+
 	// Update is called once per frame
 	void Update () {
-		
+
+		// This code makes the character jump
+		if(Input.GetKeyDown (KeyCode.Space)&& Grounded){
+			Jump();
+		}
+
+		// This code makes the character move from side to side using the A&D keys
+		if(Input.GetKey (KeyCode.D)){
+			GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			
+		}
+		if(Input.GetKey (KeyCode.A)){
+			GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			
+		}
+
+	}
+
+	public void Jump(){
+		GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, JumpHeight);
 	}
 }
