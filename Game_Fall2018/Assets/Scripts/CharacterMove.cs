a092a868af5a374155226eff830c8ab817094544
Preston Powell <preston.powell2020@gmail.com>
2018-09-24 17:20:19 -0600

    Added Double Jump and Score Manager

--- Game_Fall2018/Assets/Scripts/CharacterMove.cs
+++ Game_Fall2018/Assets/Scripts/CharacterMove.cs
@@ -3,47 +3,68 @@ using System.Collections;
 
 public class CharacterMove : MonoBehaviour {
 
-	// Player Movement Variables
-	public int MoveSpeed;
-	public float JumpHeight;
+	//Player movement variables
+	public float moveSpeed;
+	public float jumpHeight;
+	private bool doubleJump;
 
-	// Player grounded variables
-	public Transform GroundCheck;
-	public float GroundCheckRadius;
-	public LayerMask WhatIsGround;
-	private bool Grounded;
+	//Player grounded variables
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	public LayerMask whatIsGround;
+	private bool grounded;
 
+	//Non-Stick Player
+	private float moveVelocity;
+
+	
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
-	
 
 	void FixedUpdate () {
-		Grounded = Physics2D.OverlapCircle(GroundCheck.position, GroundCheckRadius, WhatIsGround);
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
 	}
 
 	// Update is called once per frame
 	void Update () {
 
 		// This code makes the character jump
-		if(Input.GetKeyDown (KeyCode.Space)&& Grounded){
+		if(Input.GetKeyDown (KeyCode.Space)&& grounded){
 			Jump();
 		}
 
+		// Double jump code
+		if(grounded)
+			doubleJump = false;
+			
+
+		if(Input.GetKeyDown (KeyCode.Space)&& !doubleJump && !grounded){
+			Jump();
+			doubleJump = true;
+		}
+
+		//Non-Stick Player
+		moveVelocity = 0f;
+
 		// This code makes the character move from side to side using the A&D keys
 		if(Input.GetKey (KeyCode.D)){
-			GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-			
+			// GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			moveVelocity = moveSpeed;
 		}
+		
 		if(Input.GetKey (KeyCode.A)){
-			GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-			
+			//GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			moveVelocity = -moveSpeed;
 		}
+
+		GetComponent<Rigidbody2D>().velocity = new Vector2(moveVelocity, GetComponent<Rigidbody2D>().velocity.y);
+
 
 	}
 
 	public void Jump(){
-		GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, JumpHeight);
+		GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
 	}
 }
---
+++ Game_Fall2018/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour {
+
+	public static int Score;
+
+	Text ScoreText;
+
+	// Use this for initialization
+	void Start () {
+		ScoreText = GetComponent<Text>();
+
+		Score = 0;
+ 	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Score < 0)
+			Score = 0; 
+		ScoreText.text = " " + Score;
+	}
+
+	public static void AddPoints (int PointsToAdd) {
+		Score += PointsToAdd;
+	}
+
+	// public static void Reset () {
+	// 	Score=0;
+	// }
+
+}
