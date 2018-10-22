60d8b6e42699ed56440282d3b11e0ca5020170f3
Preston Powell <preston.powell2020@gmail.com>
2018-10-22 17:18:15 -0600

    Added projectile and kill enemy

---
+++ Game_Fall2018/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShoot : MonoBehaviour {
+	// Shoot Variables
+	public Transform FirePoint;
+	public GameObject Projectile;
+	
+	void Start(){
+		//Projectile = GameObject.Find("Projectile"); 
+	}
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.RightControl))
+			Instantiate(Projectile,FirePoint.position, FirePoint.rotation);
+		
+	}
+}
---
+++ Game_Fall2018/Assets/Scripts/Projectile.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+	public float Speed;
+	public Rigidbody2D PC;
+
+	public GameObject EnemyDeath;
+
+	public GameObject ProjectileParticle;
+
+	public int PointsForKill;
+
+	// Use this for initialization
+	void Start () {
+		 
+		if(PC.transform.localScale.x < 0)
+			Speed = -Speed;
+
+			// Speed = Speed * Mathf.Sign(PC.transform.localScale.x);
+
+			// GetComponent<Rigidbody2D>().velocity = new Vector2(Speed + (PC.GetComponent<Rigidbody2D>().velocity.x/3),GetComponent<Rigidbody2D>().velocity.y + (PC.GetComponent<Rigidbody2D>().velocity.y/3));
+
+			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Rigidbody2D>().velocity = new Vector2(Speed, GetComponent<Rigidbody2D>().velocity.y);
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.tag == "Enemy"){
+			Instantiate(EnemyDeath, other.transform.position, other.transform.rotation);
+			Destroy (other.gameObject);
+			ScoreManager.AddPoints (PointsForKill);
+		}
+		
+		
+		Instantiate(ProjectileParticle, transform.position, transform.rotation);
+		Destroy (gameObject);
+	}
+}
+ 
