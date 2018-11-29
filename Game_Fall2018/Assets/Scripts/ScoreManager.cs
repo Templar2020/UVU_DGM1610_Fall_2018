f79872ae572421ab21613755355118db01ebb03b
Preston Powell <preston.powell2020@gmail.com>
2018-11-28 17:11:44 -0700

    Added Win UI Text and Win Scenerio in Scoremanager Script

--- Game_Fall2018/Assets/Scripts/ScoreManager.cs
+++ Game_Fall2018/Assets/Scripts/ScoreManager.cs
@@ -1,18 +1,28 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class ScoreManager : MonoBehaviour {
 
 	public static int Score;
+	public int WinScore;	
+
+	public Text WinText;
 
 	Text ScoreText;
 
+	void Awake(){
+		Time.timeScale = 1;
+	}
+	
 	// Use this for initialization
 	void Start () {
 		ScoreText = GetComponent<Text>();
 
 		Score = 0;
+
+		WinText.GetComponent<Text>().enabled = false;
  	}
 	
 	// Update is called once per frame
@@ -20,6 +30,18 @@ public class ScoreManager : MonoBehaviour {
 		if (Score < 0)
 			Score = 0; 
 		ScoreText.text = " " + Score;
+
+		// If the player win display win text
+		if(Score >= WinScore ){
+			print("Win Score Reached = " + Score);
+			WinText.GetComponent<Text>().enabled = true;
+			Time.timeScale = 0;
+		}
+		// If player hits the Escape key return to start menu
+		if(Input.GetKeyDown(KeyCode.Escape)){
+			SceneManager.LoadScene(0);
+		}
+
 	}
 
 	public static void AddPoints (int PointsToAdd) {
@@ -28,8 +50,6 @@ public class ScoreManager : MonoBehaviour {
 	
 	}
 	
-	// public static void Reset () {
-	// 	Score=0;
-	// }
+	
 
 }
